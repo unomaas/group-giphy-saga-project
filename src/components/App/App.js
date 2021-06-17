19ba46b46fb6b641b9d400867650f52157fd8b5b
Ryan Maas <rdmjobs@live.com>
2021-06-17 13:15:45 -0500

    importing correctly.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,9 @@
 import React from 'react';
+import Header from '../Header/Header';
+import SearchView from '../SearchView/SearchView';
+import SearchItem from '../SearchItem/SearchItem';
+import FavoritesView from '../FavoritesView/FavoritesView';
+import FavoritesItem from '../FavoritesItem/FavoritesItem';
 
 function App(props) {
   return (
