bd5c0c75c898879577b8193c727845aa7ff4735d
Ryan Maas <rdmjobs@live.com>
2021-06-17 10:21:25 -0500

    All dependencies setup.

--- database.sql
+++ database.sql
@@ -1,7 +1,20 @@
 CREATE DATABASE "giphy_search_favorites";
+-- DATABASE WIREFRAME: https://lucid.app/lucidchart/b6123d9a-de7b-4f7e-94e6-833e1f293a31/edit?viewport_loc=-11%2C-11%2C3072%2C1545%2C0_0&invitationId=inv_d56c1f37-9a3a-4210-8524-f463205d58b0
 
 -- You'll need a table for storing each giphy image favorite
 -- Each favorite image can be assigned 1 of the following categories as a Foreign Key
+
+-- Favorite table
+CREATE TABLE "favorites" (
+	"id" SERIAL PRIMARY KEY,
+	"gif_url" VARCHAR(255) NOT NULL,
+	"category_id" INT REFERENCES "category"
+);
+
+-- Sample data model
+INSERT INTO "favorites" ("gif_url", "category_id")
+VALUES ('sample-gif-url.giphy', 1);
+
 
 -- Category table
 CREATE TABLE "category" (
--- src/index.js
+++ src/index.js
@@ -1,5 +1,49 @@
+//#region ⬇⬇ All document setup, below:
+// ⬇ App Setup:
+import App from './components/App/App';
+// ⬇ React/Redux Setup:
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App/App';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+// ⬇ Middleware Setup:
+import logger from 'redux-logger';
+import createSagaMiddleware from 'redux-saga';
+// ⬇ Saga Setup:
+import { takeEvery, put } from 'redux-saga/effects';
+// ⬇ Server Calls Setup:
+import axios from 'axios';
+//#endregion ⬆⬆ All document setup above.
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+//#region ⬇⬇ All Saga functions, below:
+// ⬇ rootSaga below:
+function* rootSaga() {
+
+} // End rootSaga
+//#endregion ⬆⬆ All Saga functions above. 
+
+
+//#region ⬇⬇ All Reducer functions, below:
+const dummyReducer = (state = [], action) => {
+  return state;
+}
+//#endregion ⬆⬆ All Reducer functions above. 
+
+
+//#region ⬇⬇ All Store & Middleware setup, below:
+// ⬇ Create sagaMiddleware:
+const sagaMiddleware = createSagaMiddleware();
+
+// ⬇ Create store:
+const store = createStore(
+  combineReducers({ dummyReducer }),
+  applyMiddleware(sagaMiddleware, logger),
+);
+
+// ⬇ Pass rootSaga into our sagaMiddleware:
+sagaMiddleware.run(rootSaga);
+
+// ⬇ Rendering:
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
+//#endregion ⬆⬆ All Store & Middleware setup above.
